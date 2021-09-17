78f927ea9de84364794b0a6218ba6e5437f81a24
Adam Abrams <adam.abrams@optum.com>
2021-09-17 14:28:31 -0500

    fix: improve build script error message

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+which go >/dev/null ||
+    { echo "cannot build without 'go' installed"; exit 2; }
+
 go build -o bin/ || { echo "failure"; exit 1; }
 echo "build successful"
 echo "path to the flatnvim binary: $PWD/bin/flatnvim"
